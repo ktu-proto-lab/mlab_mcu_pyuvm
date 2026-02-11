042303046e41086a1ba3c238e86c5dac8f8810af
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-02-11 12:59:36 +0200

    refac: more robust patcher

--- patch/patcher.sh
+++ patch/patcher.sh
@@ -3,19 +3,32 @@
 readonly CALL_PATH="$(pwd)"
 readonly PROJECT_ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../.."
 
-OPT_APPLY_PATCHES="--apply"
-OPT_RESTORE_PATCHES="--restore"
+readonly OPT_APPLY_PATCHES="--apply"
+readonly OPT_REVERSE_PATCHES="--reverse"
+
+if [[ $# -eq 0 ]]; then
+    cat <<EOF
+Usage:
+    ./patcher.sh [options]
+
+Options:
+    $OPT_APPLY_PATCHES:     apply patches
+    $OPT_REVERSE_PATCHES:   restore patches (return to untouched state)
+
+EOF
+    exit 1
+fi
 
 APPLY_PATCHES=false
-RESTORE_PATCHES=false
+REVERSE_PATCHES=false
 
 while [ $# -gt 0 ]; do
     case $1 in
     "$OPT_APPLY_PATCHES")
         APPLY_PATCHES=true
         ;;
-    "$OPT_RESTORE_PATCHES")
-        RESTORE_PATCHES=true
+    "$OPT_REVERSE_PATCHES")
+        REVERSE_PATCHES=true
         ;;
     -*)
       echo "[  ERROR]: Unknown provided option $1"
@@ -29,10 +42,12 @@ cd "$PROJECT_ROOT"
 
 if $APPLY_PATCHES; then
     # EEPROM compatibility with cocotb
-    patch ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
+    patch -Nfs -V none -r - ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
+    echo "[   INFO]: Patches applied"
 
-elif $RESTORE_PATCHES; then
-    patch -R ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
+elif $REVERSE_PATCHES; then
+    patch -Rfs -V none -r - ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
+    echo "[   INFO]: Returned to previous state"
 fi
 
 cd "$CALL_PATH"
