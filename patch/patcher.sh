7d5fb688108c50185c2017d2c258b56ab27cc6ad
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-02-23 14:25:43 +0200

    fix: patcher script no longer prints out failed patches and handles failures automatically

--- patch/patcher.sh
+++ patch/patcher.sh
@@ -6,6 +6,17 @@ readonly PROJECT_ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/nu
 readonly OPT_APPLY_PATCHES="--apply"
 readonly OPT_REVERSE_PATCHES="--reverse"
 
+function apply_patch {
+    # >/dev/null    - suppress stdout
+    # >&1           - suppress stderr
+    # || true       - ignore non-zero exit status (script does not fail)
+    patch -Nfs -V none -r - "$1" < "$2" >/dev/null 2>&1 || true
+}
+
+function reverse_patch {
+    patch -Rfs -V none -r - "$1" < "$2" >/dev/null 2>&1 || true
+}
+
 if [[ $# -eq 0 ]]; then
     cat <<EOF
 Usage:
@@ -41,13 +52,17 @@ done
 cd "$PROJECT_ROOT"
 
 if $APPLY_PATCHES; then
-    # EEPROM compatibility with cocotb
-    patch -Nfs -V none -r - ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
-    patch -Nfs -V none -r - ./sim/rtl/files.f < ./uvm/patch/rtl_files.patch
+    # EEPROM compatibility with cocotb (to work on Verilator)
+    apply_patch ./tb/misc/24CS512.sv  ./uvm/patch/eeprom.patch
+
+    # Avoiding relative paths to RTL source files (to work on XCelium)
+    apply_patch ./sim/rtl/files.f     ./uvm/patch/rtl_files.patch
+
     echo "[   INFO]: Patches applied"
 
 elif $REVERSE_PATCHES; then
-    patch -Rfs -V none -r - ./tb/misc/24CS512.sv < ./uvm/patch/eeprom.patch
+    reverse_patch ./tb/misc/24CS512.sv  ./uvm/patch/eeprom.patch
+    reverse_patch ./sim/rtl/files.f     ./uvm/patch/rtl_files.patch
     echo "[   INFO]: Returned to previous state"
 fi
 
