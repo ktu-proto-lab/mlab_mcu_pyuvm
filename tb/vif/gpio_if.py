2c123b30cd1622d23287e29a3b9af452dd69d713
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-03-04 20:40:24 +0200

    feat: gpio_if has own logger

--- tb/vif/gpio_if.py
+++ tb/vif/gpio_if.py
@@ -1,3 +1,4 @@
+from cocotb.log import SimLog
 from cocotb.handle import SimHandleBase
 
 class gpio_if:
@@ -11,19 +12,28 @@ class gpio_if:
 
     PIN_NUM: int = 8
 
-    def __init__(self, dut: SimHandleBase):
+    def __init__(self, dut: SimHandleBase, name="gpio_if", parent=None):
         self.clk: SimHandleBase = dut.clk
         
         self.gpio_i: SimHandleBase = dut.gpio_i
         self.gpio_o: SimHandleBase = dut.gpio_o
         self.gpio_oe: SimHandleBase = dut.gpio_oe
         
+        self.parent_name = parent.get_full_name() if hasattr(parent, "get_full_name") else "cocotb"
+        self.full_name = f"{self.parent_name}.{name}"
+        self.logger = SimLog(self.full_name)
 
     def drive_pins(self, value) -> None:
         self.gpio_i.value = value
         
     def read_pins(self) -> int:
-        return self.gpio_o.value.integer
+        if not self.gpio_o.value.is_resolvable:
+            self.logger.warning(
+                    f"GPIO pin signals contain unresolvable values: {self.gpio_o.value.binstr}, returning 0")
+            return 0
+        else:
+            return self.gpio_o.value.integer
+
 
     def read_pins_binstr(self) -> str:
         return f"0b{self.read_pins():0{self.PIN_NUM}b}"
