6e752acfce141ceea2163ce0713fcec9377a9726
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-03-07 16:59:53 +0200

    refac: report script t obetter isolate non-project files and include docker and yaml to the code report

--- script/report.sh
+++ script/report.sh
@@ -3,17 +3,16 @@
 readonly PROJECT_ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 
 function code_line_metrics {
-    cloc --quiet                                                            \
-        --match-f='(\.(c|h|v|sv|sh|py)$|^makefile$)' "$PROJECT_ROOT" |      \
+    cloc --quiet \
+        --exclude-dir="__pycache__,sim_build,build,verilator,xcelium,.venv" \
+        --match-f='(\.(c|h|v|sv|sh|py|yml)$|^makefile$|^dockerfile)' "$PROJECT_ROOT" | \
         grep -v "github.com"
 }
 
 function project_structure {
     tree "$PROJECT_ROOT"    \
         --dirsfirst         \
-        -I '__pycache__'    \
-        -I '__init__.py'    \
-        -I 'readme.md' |    \
+        -I '__pycache__|__init__.py|sim_build|build|readme.md' |    \
         sed "1s|^.*$|uvm|"
 }
 
