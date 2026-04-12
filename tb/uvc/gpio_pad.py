2817003d2d8cf4ac83231d4d4ba1a32797a9ed21
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-04-12 09:03:41 +0300

    feat: add vsc randomization for gpio pad transaction

--- tb/uvc/gpio_pad.py
+++ tb/uvc/gpio_pad.py
@@ -1,11 +1,35 @@
+import warnings
+# Disable VSC's inner warnings to not cluster the log output of the simulation
+warnings.filterwarnings(action="ignore", category=DeprecationWarning, module="vsc")
+
+import vsc
 from pyuvm import uvm_sequence_item
 
+@vsc.randobj
 class GpioPad(uvm_sequence_item):
+    prev_state = -1
+
     def __init__(self, name="GpioPad"):
         super().__init__(name)
         self.state = 0x00
         self.mask = 0xFF
         self.uart_mask = 0b0000_0011
+        self.pin_count = 8
 
     def __str__(self):
         return hex(self.state & self.mask)
+
+    @vsc.constraint
+    def c_pad_size(self):
+        self.state in vsc.rangelist(vsc.rng(0, 2**self.pin_count - 1))
+
+    @vsc.constraint
+    def c_no_sequencial_repeat_state(self):
+        self.state != GpioPad.prev_state
+
+    # vsc hook that runs automatically after self.randomize()
+    def post_randomize(self):
+        GpioPad.prev_state = self.state
+
+    def __eq__(self, other):
+        return self.state == other.state
