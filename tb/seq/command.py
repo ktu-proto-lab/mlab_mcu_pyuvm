8fa5b4836ce551e567a6f891e5c466377f35a730
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-04-15 13:53:07 +0300

    fix: generated random write value is uint instead of int

--- tb/seq/command.py
+++ tb/seq/command.py
@@ -14,15 +14,15 @@ class CommandEnum(Enum):
     test = 5
 
 @vsc.randobj
-class Command(uvm_sequence_item):    
+class Command(uvm_sequence_item):
     def __init__(self, name="Command"):
         super().__init__(name)
         self.cmd = vsc.rand_enum_t(CommandEnum)
         self.addr = vsc.rand_uint32_t()
-        self.value = vsc.rand_int32_t()
+        self.value = vsc.rand_uint32_t()
         self.word_count = vsc.rand_uint32_t()
-            
-        
+
+
     @vsc.constraint
     def c_mem_addr_space(self):
         imem_base_addr = 0x80000000
@@ -33,7 +33,7 @@ class Command(uvm_sequence_item):
             vsc.weight(vsc.rng(dmem_base_addr, dmem_base_addr + dmem_size_bytes), 50),
             vsc.weight(vsc.rng(imem_base_addr, imem_base_addr + imem_size_bytes), 50)
         ])
-        
+
     @vsc.constraint
     def c_cmd_write_value_dist(self):
         # check min and max values, and 0
@@ -47,22 +47,22 @@ class Command(uvm_sequence_item):
                 vsc.weight(vsc.rng(1, int32_t_max_value), 30),
                 vsc.weight(vsc.rng(int32_t_min_value, -1), 30)
             ])
-            
+
     @vsc.constraint
     def c_word_count_range(self):
         with vsc.if_then((self.cmd == CommandEnum.cksum) | (self.cmd == CommandEnum.dump)):
             self.word_count in vsc.rangelist(vsc.rng(1, 16))
-        
+
     @vsc.constraint
     def c_cmd_equal_dist(self):
         vsc.dist(self.cmd,[
-            vsc.weight(CommandEnum.read, 20),
-            vsc.weight(CommandEnum.write, 20),
-            vsc.weight(CommandEnum.cksum, 20),
-            vsc.weight(CommandEnum.dump, 20),
-            vsc.weight(CommandEnum.test, 20)
+            vsc.weight(CommandEnum.read, 0),
+            vsc.weight(CommandEnum.write, 100),
+            vsc.weight(CommandEnum.cksum, 0),
+            vsc.weight(CommandEnum.dump, 0),
+            vsc.weight(CommandEnum.test, 0)
         ])
-        
+
     def to_string(self) -> str:
         if self.cmd == CommandEnum.read:
             return f"mem read {hex(self.addr)}"
