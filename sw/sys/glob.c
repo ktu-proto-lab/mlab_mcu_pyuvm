9e6f20f68de983890c8c5ff3fccec31a6218a910
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-05-07 16:07:31 +0300

    fix: set cpu frequency to 50MHz

--- sw/sys/glob.c
+++ sw/sys/glob.c
@@ -3,13 +3,11 @@
 volatile uart_handle_t g_uart;
 volatile gpio_handle_t g_gpio;
 
-const uint32_t g_cpu_freq_mhz = 80;
+const uint32_t g_cpu_freq_mhz = 50;
 const uint32_t g_uart_baud_rate = 115200;
 
 void g_init(void) {
     gpio_init(&g_gpio);
-    const uint32_t g_cpu_freq_mhz = 80;
-    const uint32_t g_uart_baud_rate = 115200;
     g_uart.config.baud_rate = UART_BAUD_INTERVAL(g_cpu_freq_mhz, g_uart_baud_rate);
     g_uart.config.word_length = 0;
     g_uart.config.stop_bits = 0;
@@ -19,7 +17,7 @@ void g_init(void) {
     uart_init(&g_uart);
     g_gpio.regs->aux  |= UART_GPIO_TX_PIN;
     g_gpio.regs->oe   |= UART_GPIO_TX_PIN;
-    g_gpio.regs->oe   |= SYSTEM_STATE_MASK;
+    g_gpio.regs->oe   |= GPIO_PIN_ALL;
     uart_tx_enable(&g_uart);
     // BUG: trying to enable rx like this stalls the test
     // uart_rx_enable(&uart);
