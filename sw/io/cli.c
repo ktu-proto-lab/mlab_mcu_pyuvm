2469e535dc850fdd3717f19c329809e0883cabd3
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-06-03 12:05:12 +0300

    feat: gpio animation on basys3 fpga cli command

--- sw/io/cli.c
+++ sw/io/cli.c
@@ -6,6 +6,7 @@
 #include "sys/mem.h"
 #include "sys/err.h"
 #include "hal/gpio.h"
+#include "sys/time.h"
 
 static system_error_t cli_echo_handler(int argc, char **argv) {
     if (argc != CLI_CMD_ECHO_ARG_COUNT) {
@@ -236,6 +237,32 @@ static system_error_t cli_gpio_toggle_handler(int argc, char **argv) {
     return SYSTEM_ERROR_NONE;
 }
 
+static void cli_gpio_animation_loop(uint32_t milliseconds) {
+    for (uint32_t i = GPIO_PIN_5; i <= GPIO_PIN_7; i = (i << 1)) {
+        g_gpio.regs->out = i;
+        time_delay_microseconds(milliseconds * 1000, g_cpu_freq_mhz);
+    }
+}
+
+static void cli_gpio_animation(uint32_t loop_count) {
+    const uint32_t base_delay_ms = 5000;
+    for (uint32_t i = 0; i < loop_count; ++i) {
+        cli_gpio_animation_loop(base_delay_ms >> i);
+    }
+}
+
+static system_error_t cli_gpio_animation_handler(int argc, char **argv) {
+    uint32_t loop_count;
+    if (!string_hex_to_uint(argv[2], &loop_count)) {
+        // TODO
+        return SYSTEM_ERROR_DEFAULT;
+    }
+    printf("gpio animation started\n");
+    cli_gpio_animation(loop_count);
+    printf("gpio animation done\n");
+    return SYSTEM_ERROR_NONE;
+}
+
 static system_error_t cli_gpio_handler(int argc, char **argv) {
     const char *subcmd = argv[1];
     if (string_compare(subcmd, "get") == 0) {
@@ -244,6 +271,8 @@ static system_error_t cli_gpio_handler(int argc, char **argv) {
         return cli_gpio_set_handler(argc, argv);
     } else if (string_compare(subcmd, "toggle") == 0) {
         return cli_gpio_toggle_handler(argc, argv);
+    } else if (string_compare(subcmd, "-a") == 0) {
+        return cli_gpio_animation_handler(argc, argv);
     } else {
         return SYSTEM_ERROR_CLI_CMD_MEM_SUBCMD_NOT_FOUND;
     }
